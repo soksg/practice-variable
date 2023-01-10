f5c1eaf1659131df5591407196a3a23abf98ba36
EC2 Default User <sk2361ksg@gmail.com>
2023-01-10 01:28:56 +0000

    [Start]関数の学習

--- sample.js
+++ sample.js
@@ -1,4 +1,12 @@
-for (let i=0; i<10; i++){
-  console.log(i)
+// メイン部分
+let alertString;
+// 作成した関数を呼び出し、変数へ格納
+alertString=addString("WebCamp");
+// 変数の中身をアラートで表示する
+alert(alertString);
+
+// 関数
+function addString(strA){
+  let addStr="Hello " + strA;
+  return addStr;
 }
-  console.log(i)
