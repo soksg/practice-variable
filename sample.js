cc352005c8c821ef4b82d332a8555f895aa6cf0e
EC2 Default User <sk2361ksg@gmail.com>
2023-01-10 01:34:26 +0000

    [Add]入力ダイアログ作成

--- sample.js
+++ sample.js
@@ -1,12 +1,2 @@
-// メイン部分
-let alertString;
-// 作成した関数を呼び出し、変数へ格納
-alertString=addString("WebCamp");
-// 変数の中身をアラートで表示する
-alert(alertString);
-
-// 関数
-function addString(strA){
-  let addStr="Hello " + strA;
-  return addStr;
-}
+let promptStr=prompt("何か好きな文字を入力してください。")
+alert(promptStr);
