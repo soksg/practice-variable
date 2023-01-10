40fd4d5623d209031d3105f5d97c64f86c4112bb
EC2 Default User <sk2361ksg@gmail.com>
2023-01-10 08:12:15 +0000

    [Finish]じゃんけんプログラム作成

--- sample.js
+++ sample.js
@@ -2,17 +2,17 @@
 let user_hand=prompt('じゃんけんの手を、グー、チョキ、パーの中から選んでください。');
 
 // じゃんけんの手をランダムに作成する関数を呼び出す
-let js_hand=get.JShand();
+let js_hand=getJShand();
 
 // ユーザーの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
 let judge=winlose(user_hand, js_hand);
 
 // 結果を表示する
-alert("あなやの選んだ手は、" + user_hand,+ "\nJavaScriptの選んだ手は" + js_hand + "です。\n結果は" + judge + "です。");
+alert('あなやの選んだ手は、' + user_hand + '\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
 
 // ランダムでじゃんけんの手を作成する関数
-function get.JShand(){
-  let js_hand==Math.floor(Math.ramdom()*3);
+function getJShand(){
+  let js_hand_num=Math.floor(Math.random()*3);
   let hand_name;
 
   if (js_hand_num==0){
@@ -26,7 +26,7 @@ function get.JShand(){
   return hand_name;
 }
 
-ユーザーの手とJavaScriptのじゃんけんの手を比べる関数
+// ユーザーの手とJavaScriptのじゃんけんの手を比べる関数
 function winlose(user, js){
   let winloseStr;
 
@@ -44,7 +44,7 @@ function winlose(user, js){
       } else if (js=="チョキ"){
         winloseStr="あいこ";
       } else if (js=="パー"){
-        winloseStr="勝ち"
+        winloseStr="勝ち";
       }
   } else if(user=="パー"){
       if (js=="グー"){
